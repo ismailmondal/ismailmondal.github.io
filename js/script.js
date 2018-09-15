a24dfdc69caf1b10b6d3fbecd65203d4ab230b0d
ismailmondal <ismailmondal220@gmail.com>
2018-09-15 11:18:03 +0530

    15-9-18

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 (function($) {	
 	"use strict";
+
+	$.fn.visible = function(partial) {    
+      var $t            = $(this),
+          $w            = $(window),
+          viewTop       = $w.scrollTop(),
+          viewBottom    = viewTop + $w.height(),
+          _top          = $t.offset().top,
+          _bottom       = _top + $t.height(),
+          compareTop    = partial === true ? _bottom : _top,
+          compareBottom = partial === true ? _top : _bottom;    
+    	return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    };
 	
 	//Hide Loading Box (Preloader)
 	function Preloader() {
@@ -23,14 +35,16 @@
 			}
 		}
 	}	
-	headerStyle();
+	//headerStyle();
 
+	
 /* ===============================================
    When document is Scrollig
 ==============================================*/
 	
 	$(window).on('scroll', function() {
 		headerStyle();
+		count();
 	});
 		
 /* =========================================
